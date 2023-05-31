b7585c1a9582b983b8486ee5d6533756eb313a71
coro1x00 <110147950+coro1x00@users.noreply.github.com>
2023-05-31 22:34:59 +0700

    Update banget.py

--- banget.py
+++ banget.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-# @title ***CHROME***
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+#@title ***SELENIUM SOCKS5***
+from seleniumwire import webdriver
+import time
 
-# Opciones de Chrome
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
+options = {
+        'proxy': {
+            'http": "http://heukqttc-rotate:31exp7lxvazt@p.webshare.io:80/', 
+            'https": "http://heukqttc-rotate:31exp7lxvazt@p.webshare.io:80/',
+            'no_proxy': 'localhost,127.0.0.1'
+        }  
+    }
 
-# Ruta del controlador de Chrome
-chrome_driver_path = '/content/chromedriver'
 
-# Crear un objeto Service para especificar la ruta del controlador
-service = Service(chrome_driver_path)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
 
-# Crear una instancia del navegador Chrome
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-# Navegar a una página web
+driver = webdriver.Chrome('chromedriver', options=chrome_options,seleniumwire_options=options)
 driver.get("https://shifu.mrpandabear.org/mine?wallet=002CA3573BA9FB3CEDB451A4D85E5A7D29DF5D2000DCC9AD27")
 print(driver.title)
-time.sleep (999999)
+time.sleep(999999)
